7ed5d3b87d9e236c64b939a4deaca27ad95c54b4
Liz3 <accs@liz3.net>
2021-12-07 18:36:14 +0100

    add two keywords to c++

--- src/languages.h
+++ src/languages.h
@@ -5,7 +5,7 @@
 #include "la.h"
 const Language CPP = {
   "C++",
-  {"for", "while", "if", "int", "float", "return", "double", "true", "false", "else", "void", "delete", "struct", "class", "case", "break", "const", "nullptr", "auto", "bool", "new", "auto*", "switch", "case"},
+  {"for", "while", "if", "int", "float", "return", "double", "true", "false", "else", "void", "delete", "struct", "class", "case", "break", "const", "nullptr", "auto", "bool", "new", "auto*", "switch", "case", "typedef", "static"},
   {"#include", "std::", "#ifdef", "#define", "#endif", "#ifndef"},
   "//",
   {"/*", "*/"},
