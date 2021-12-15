d624f2c799fde07c9cf37d6d4dd638a402c971c7
Liz3 <accs@liz3.net>
2021-12-15 21:52:50 +0100

    added async as js keyword

--- src/languages.h
+++ src/languages.h
@@ -17,7 +17,7 @@ const std::vector<Language> LANGUAGES = {
 },
 {
   "JavaScript",
-  {"null", "undefined", "as", "from", "abstract","arguments","await","boolean","break","byte","case","catch","char","class","const","continue","debugger","default", "let","delete","do","double","else","enum","eval","export","extends","false","final","finally","float","for","function","goto","if","implements","import","in","instanceof","int","interfacelet","long","native","new","null","package","private","protected","public","return","short","static","super","switch","synchronized","this","throw","throws","transient","true","try","typeof","var","void","volatile","while","with","yield"},
+  {"async", "null", "undefined", "as", "from", "abstract","arguments","await","boolean","break","byte","case","catch","char","class","const","continue","debugger","default", "let","delete","do","double","else","enum","eval","export","extends","false","final","finally","float","for","function","goto","if","implements","import","in","instanceof","int","interfacelet","long","native","new","null","package","private","protected","public","return","short","static","super","switch","synchronized","this","throw","throws","transient","true","try","typeof","var","void","volatile","while","with","yield"},
   {"process", "requestAnimationFrame", "window", "console", "Object", "Array", "Number", "String", "JSON", "Buffer", "require"},
   "//",
   {"/*", "*/"},
