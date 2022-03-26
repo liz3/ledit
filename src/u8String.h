ecf6dfbd6637323332c01d54f30fac7520f7a0dc
Liz3 <accs@liz3.net>
2022-03-26 08:13:31 +0100

    fix: treat binary files as ascii.
    There was a issue where binary content would crash within the convert to wide char.
    This now uses C++ runtime exceptions to catch that and return the plain content rather then crashing.

--- src/u8String.h
+++ src/u8String.h
@@ -18,8 +18,19 @@ static std::string convert_str(std::u16string data) {
 }
 
 static std::u16string create(std::string container) {
-  std::u16string b = utfConverter.from_bytes(container);
-  return b;
+  try {
+    std::u16string b = utfConverter.from_bytes(container);
+    return b;
+  } catch(std::exception e) {
+    // failed to convert treat at ascii to display something at least
+    std::u16string target = u"";
+    std::string::const_iterator c;
+    for (c = container.begin(); c != container.end(); c++) {
+      target += (char16_t)*c;
+    }
+    return target;
+}
+
 }
 std::u16string numberToString(int value) {
   std::string val = std::to_string(value);
