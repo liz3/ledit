8c928b5362271c9da8478e753f0b1f817454175c
Liz3 (Yann HN) <accs@liz3.net>
2023-12-10 02:08:04 +0100

    fix dockerfile extension

--- src/languages.h
+++ src/languages.h
@@ -103,7 +103,7 @@ const std::vector<Language> LANGUAGES = {
      {"", ""},
      "\"",
      '\\',
-     {"dockerfile"}},
+     {"dockerfile", "Dockerfile"}},
     {"Elixir",
      {"def",
       "defp",
